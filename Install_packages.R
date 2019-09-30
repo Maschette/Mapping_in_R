d150a01d25b151425e68f62d54101f7db0d94e16
Dale Maschette <dale.maschette@aad.gov.au>
2019-09-30 22:29:01 +1000

    Add Install list

---
+++ Install_packages.R
@@ -0,0 +1,5 @@
+install.packages(c("remotes","tidyverse","sf", "maps", "sp", "maptools", "raster"), dependencies = TRUE)
+remotes::install_github("clauswilke/relayer", dependencies = TRUE)
+remotes::install_github("AustralianAntarcticDivision/SOmap", ref = "dev-0.5", dependencies = TRUE).
+remotes::install_github("eliocamp/ggnewscale", dependencies = TRUE)
+remotes::install_github("SCAR-sandpit/quantarcticR", dependencies = TRUE)
